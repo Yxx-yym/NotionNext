297efd7e00ceeaf2c44b85060b21e69dd7929462
Jade_Qiu <52923914+Yxx-yym@users.noreply.github.com>
2022-05-10 13:25:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: '超级英专生', // 作者
   BIO: '几个普通的干饭人🍚', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  LINK: 'https://gxpt-hfut.vercel.app', // 网站地址
   AVATAR: '/avatar.png', // 个人头像 默认取public目录下的avatar.png
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
