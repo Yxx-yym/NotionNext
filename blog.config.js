65a05926b0dc632d6589a7b82f259b993ed37da1
Jade_Qiu <52923914+maybemerald@users.noreply.github.com>
2022-06-22 23:17:02 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -43,7 +43,7 @@ const BLOG = {
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'mail@tangly1024.com',
+  CONTACT_EMAIL: '',
   CONTACT_WEIBO: '',
   CONTACT_TWITTER: '',
   CONTACT_GITHUB: '',
